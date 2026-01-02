14dc42acf1b8bff8ab8b25b8bab7a065f6f1b00e
nikolaos.karberis <nikolaos.karberis@gmail.com>
2026-01-02 15:43:54 +0200

    feat: exercise: length detector

--- src/gr/aueb/cf/testgit/Strings/LengthDetector.java
+++ src/gr/aueb/cf/testgit/Strings/LengthDetector.java
@@ -8,12 +8,13 @@ import java.util.Scanner;
 public class LengthDetector {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        final int LENGTH_LIMIT = 10;
         String word = "";
 
         System.out.println("Please enter the word for length detection: ");
         word = scanner.nextLine();
 
-        if (word.length() > 10){
+        if (word.length() > LENGTH_LIMIT){
             System.out.println("The word is longer than 10 characters.");
         } else{
             System.out.println("The word is shorter than 10 characters.");
