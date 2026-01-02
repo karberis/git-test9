6be6d9fbcfe016f37d6a14b0cf80dad8281559d2
nikolaos.karberis <nikolaos.karberis@gmail.com>
2026-01-02 09:42:22 +0200

    feat: traverse an array using for loop and for-each

---
+++ src/gr/aueb/cf/testgit/TraverseArray.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.cf9.exercises;
+
+/**
+ * Traverse an array using for loop and for-each
+ */
+
+public class TraverseArray {
+    static void main() {
+
+        int [] ages = {47, 22, 19, 15, 65};
+
+        // for loop
+        System.out.print("For loop: ");
+        for( int i = 0; i < ages.length; i++){
+            System.out.print(ages[i] + " ");
+        }
+
+        System.out.println();
+
+        // for-each loop
+        System.out.print("For-each loop: ");
+        for(int age: ages) {
+            System.out.print(age + " ");
+        }
+
+    }
+}
