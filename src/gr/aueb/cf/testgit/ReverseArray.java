a15a08b99e4a9a39bd6f91f8ea2e62af60ea1730
nikolaos.karberis <nikolaos.karberis@gmail.com>
2026-01-02 10:11:18 +0200

    feat: reverse an array using for loop

---
+++ src/gr/aueb/cf/testgit/ReverseArray.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.cf9.exercises;
+
+/**
+ * Reverse an array using for loop
+ */
+public class ReverseArray {
+    static void main() {
+        int [] ages = {47, 22, 19, 15, 65};
+
+        // traverse for loop
+        System.out.print("Ages: ");
+        for(int age : ages){
+            System.out.print(age + " ");
+        }
+
+        System.out.println();
+
+        // reverse for loop
+        System.out.print("Reverse, ages: ");
+        for(int i = ages.length - 1; i >= 0; i--){
+            System.out.print(ages[i] + " ");
+
+        }
+    }
+}
