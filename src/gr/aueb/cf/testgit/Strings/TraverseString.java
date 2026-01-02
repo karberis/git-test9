501496ef70ed100f8ddd64e7dd62b1c23da886c8
nikolaos.karberis <nikolaos.karberis@gmail.com>
2026-01-02 18:59:52 +0200

    feat: traverse a string using for and for-each loop.

---
+++ src/gr/aueb/cf/testgit/Strings/TraverseString.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.cf9.exercises.Strings;
+
+/**
+ * Traverse a string using for
+ * and for-each loop.
+ */
+public class TraverseString {
+    public static void main(String[] args) {
+        String firstname = "Nikolaos";
+        String lastname = "Karberis";
+
+        // for loop
+        for(int i = 0; i < firstname.length(); i++){
+            System.out.print(firstname.charAt(i) + " ");
+        }
+
+        System.out.println();
+
+        // for-each loop
+        for(char ch : lastname.toCharArray()){
+            System.out.print(ch + " ");
+        }
+
+    }
+}
